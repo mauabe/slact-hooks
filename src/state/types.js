68f37a4f095f30880733660b66f7a0c80257f68d
Mauricio Feldman-Abe <mauabe@gmx.com>
2019-03-16 19:45:00 -0700

    added emoji reacitons

--- src/state/types.js
+++ src/state/types.js
@@ -1,2 +1,41 @@
 export const NEW_MESSAGE = 'NEW_MESSAGE';
 export const SET_USERNAME = 'SET_USERNAME';
+
+export const REACTION_OBJECTS =[
+  {
+    type: 'REACTION_LOVE', 
+    emoji: '❤️'
+  },
+  {
+    type: 'REACTION_LIKE', 
+    emoji: '👍'
+  },
+  {
+    type: 'REACTION_DISLIKE', 
+    emoji: '👎'
+  },
+  {
+    type: 'REACTION_LAUGH', 
+    emoji: '😂'
+  },
+  {
+    type: 'REACTION_FIRE', 
+    emoji: '🔥'
+  },
+  {
+    type: 'REACTION_PARTY', 
+    emoji: '🎉'
+  },
+  {
+    type: 'REACTION_MONEY', 
+    emoji: '💰'
+  },
+  {
+    type: 'REACTION_VULCAN', 
+    emoji: '🖖'
+  },
+  {
+    type: 'REACTION_BOMB', 
+    emoji: '💣'
+  }
+]
